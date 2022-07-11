78a81b8ee530f375d1abb735e5d0b1b747774cc3
Deven Kalra <ideven85@gmail.com>
2022-07-11 13:48:27 +0530

    First Commit

---
+++ src/main/java/com/cleo/blogAPI/blogs/Blog.java
@@ -0,0 +1,94 @@
+package com.cleo.blogAPI.blogs;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.Type;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Blog {
+
+    @Id @GeneratedValue private Long id;
+
+    @Column(nullable = false)
+
+    private String title;
+
+
+    @Type(type = "text")
+    private String body;
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private Category category;
+    private Date date_created;
+
+    private Date date_updated;
+
+    private boolean status;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public Date getDate_created() {
+        return date_created;
+    }
+
+    public void setDate_created(Date date_created) {
+        this.date_created = date_created;
+    }
+
+    public Date getDate_updated() {
+        return date_updated;
+    }
+
+    public void setDate_updated(Date date_updated) {
+        this.date_updated = date_updated;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+    @PrePersist
+    public void onCreate(){
+        this.date_created = new Date();
+    }
+
+    @PreUpdate
+    public void onUpdate(){
+        this.date_updated = new Date();
+    }
+
+
+}
---
+++ src/main/java/com/cleo/blogAPI/blogs/BlogRepository.java
@@ -0,0 +1,8 @@
+package com.cleo.blogAPI.blogs;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface BlogRepository extends CrudRepository<Blog, Long> {
+
+
+}
